140c45a94a76e7401966422e2bba246df8505800
tajdakoscak <49921211+tajdakoscak@users.noreply.github.com>
2024-12-27 19:39:14 +0100

    Added Algorithm 2.1

---
+++ C_table.m
@@ -0,0 +1,47 @@
+function C_table = C_table(m, k, l)
+    % Compute the full C-table for the constrained dual Bernstein basis
+    % Inputs:
+    %   m - degree of the curve
+    %   k, l - constraints at the boundaries
+    % Outputs:
+    %   C_table - do indeksov m-l-1
+
+
+    C_table = zeros(m -l-1, m -l-1);
+
+    A = @(u) (u - m) * (u - k) * (u + k + 2) / (u + 1);
+    B = @(u) u * (u - m - l - 2) * (u - m + l) / (u - m - 1);
+
+
+    % Compute the first row (j = k+1)
+    j = k + 1;
+    for h = m - l - 1:-1:k + 1
+        if h == m - l - 1
+            C_table(j, h) = (1 / nchoosek(m, k + 1)) * ...
+                            (1 / nchoosek(m, l + 1)) * ...
+                            (-1)^(m - k - l - 2) * factorial(m + k + l + 3) / ...
+                            ((m - k - l - 2) * factorial(2 * k + 2) * factorial(2 * l + 2));
+        else
+            C_table(j, h) = ((h - m) * (h - k) * (h + k + 3)) / ...
+                            ((h + 1) * ((h - m)^2 - (l + 1)^2)) * ...
+                            C_table(j, h + 1);
+        end
+    end
+
+    for j = k+1:m-l-2
+        for h = k+1: m-k-1
+            if h == m-k-1
+                C_table(j+1, h) = 1/A(j) * (2*(j-h)*(j+h-m) * C_table(j,h) + ...
+                B(h) * C_table(j,h-1) - ...
+                B(j) * C_table(j-1,h))
+
+            else
+
+            C_table(j+1, h) = 1/A(j) * (2*(j-h)*(j+h-m) * C_table(j,h) + ...
+            B(h) * C_table(j,h-1) + ...
+            A(h) * C_table(j,h+1)- ...
+            B(j) * C_table(j-1,h))
+            end
+        end
+
+    end
