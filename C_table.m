1cbb2a56eb9fee6cacb5e78e20a2a86c7bcebc8c
tajdakoscak <49921211+tajdakoscak@users.noreply.github.com>
2024-12-28 16:52:07 +0100

    Update C_table.m

--- C_table.m
+++ C_table.m
@@ -1,26 +1,26 @@
 function C_table = C_table(m, k, l)
+    % Compute the C-table using Algorithm 2.1
     % Input:
-    %   m - stopnja Bezierjeve krivulje
-    %   k - stopnja odvoda pri t = 0
-    %   l - stopnja odvoda pri t = 1
+    %   m - degree of the Bézier curve
+    %   k - derivative degree at t = 0
+    %   l - derivative degree at t = 1
     % Output:
-    %   C_table - do indeksov m-l-1; drugje je enaka 0
+    %   C_table - coefficients matrix
 
+    % Initialize C-table with zeros
+    C_table = zeros(m +1, m + 1);
 
-    C_table = zeros(m -l-1, m -l-1);
-
+    % Helper functions for A(u) and B(u)
     A = @(u) (u - m) * (u - k) * (u + k + 2) / (u + 1);
     B = @(u) u * (u - m - l - 2) * (u - m + l) / (u - m - 1);
 
-
-    % Compute the first row (j = k+1)
     j = k + 1;
     for h = m - l - 1:-1:k + 1
         if h == m - l - 1
             C_table(j, h) = (1 / nchoosek(m, k + 1)) * ...
                             (1 / nchoosek(m, l + 1)) * ...
                             (-1)^(m - k - l - 2) * factorial(m + k + l + 3) / ...
-                            ((m - k - l - 2) * factorial(2 * k + 2) * factorial(2 * l + 2));
+                            (factorial(m - k - l - 2) * factorial(2 * k + 2) * factorial(2 * l + 2));
         else
             C_table(j, h) = ((h - m) * (h - k) * (h + k + 3)) / ...
                             ((h + 1) * ((h - m)^2 - (l + 1)^2)) * ...
@@ -28,20 +28,24 @@ function C_table = C_table(m, k, l)
         end
     end
 
-    for j = k+1:m-l-2
-        for h = k+1: m-k-1
-            if h == m-k-1
-                C_table(j+1, h) = 1/A(j) * (2*(j-h)*(j+h-m) * C_table(j,h) + ...
-                B(h) * C_table(j,h-1) - ...
-                B(j) * C_table(j-1,h))
+    for j = k + 1:m - l - 2
+        for h = k + 1:m - l - 1
+            term1 = 0; term2 = 0; term3 = 0; term4 = 0;
 
-            else
-
-            C_table(j+1, h) = 1/A(j) * (2*(j-h)*(j+h-m) * C_table(j,h) + ...
-            B(h) * C_table(j,h-1) + ...
-            A(h) * C_table(j,h+1)- ...
-            B(j) * C_table(j-1,h))
+            if h >= k + 1 && h <= m - l - 1
+                term1 = 2 * (j - h) * (j + h - m) * C_table(j, h);
             end
-        end
+            if h > k + 1
+                term2 = B(h) * C_table(j, h - 1);
+            end
+            if h < m - l - 1
+                term3 = A(h) * C_table(j, h + 1);
+            end
+            if j > k + 1
+                term4 = -B(j) * C_table(j - 1, h);
+            end
 
+            C_table(j + 1, h) = (1 / A(j)) * (term1 + term2 + term3 + term4);
+        end
     end
+end
